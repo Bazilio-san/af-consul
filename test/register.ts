850b44df007e872e54d1894e462e607af53f9264
vvmakarov <vvmakarov@corp.finam.ru>
2021-12-25 19:34:26 +0300

    debug 3

--- test/register.ts
+++ test/register.ts
@@ -1,10 +1,12 @@
 /* eslint-disable no-console */
 import initConsulAgent from './init-consul-agent';
 
-initConsulAgent().then(async ({ registerService, consulApi, serviceId }) => {
-  consulApi.consulHealthService({ service: serviceId, passing: true }).then((result: any) => {
-    registerService().then((result2: any) => {
-      // console.log(result2);
-    });
-  });
-});
+const registerInConsul = async () => {
+  const { registerService, consulUI } = await initConsulAgent();
+  const isRegistered = await registerService();
+  if (isRegistered) {
+    console.log(`Consul UI: ${consulUI}`);
+  }
+};
+registerInConsul().then((r) => r);
+export default registerInConsul;
