e6b146546f17cce44a5f5d2cc4b7c29fd5fff78d
vvmakarov <vvmakarov@corp.finam.ru>
2022-01-11 17:36:37 +0300

    22-01-11--7

--- src/constants.ts
+++ src/constants.ts
@@ -3,4 +3,4 @@ export const MAX_API_CACHED = 3;
 
 export const DEBUG = (String(process.env.DEBUG || '')).trim();
 export const CONSUL_DEBUG_ON = /\baf-consul:?\*?/i.test(DEBUG) || DEBUG === '*';
-export const { FORCE_EVERY_REGISTER_ATTEMPT } = process.env;
+export const FORCE_EVERY_REGISTER_ATTEMPT = !!process.env.FORCE_EVERY_REGISTER_ATTEMPT;
