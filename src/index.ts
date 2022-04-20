2e88a9da419a25418f5286fba631c6cb17869d7f
vvmakarov <vvmakarov@corp.finam.ru>
2022-04-20 15:17:58 +0300

    22-04-20--1

--- src/index.ts
+++ src/index.ts
@@ -4,3 +4,4 @@ export { getAPI } from './get-api';
 export { getRegisterConfig, getServiceID } from './get-register-config';
 export { getConsulApiCached, prepareConsulAPI } from './prepare-consul-api';
 export { getFQDN, getFQDNCached } from './lib/fqdn';
+export { checkAccessPointAvailability, isHttpAvailable } from './access-points/access-points-utils';
